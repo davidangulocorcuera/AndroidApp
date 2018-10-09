cd0b587f3e0aec0dfd48df3c8d8f980970f457e1
David <davidangulocorcuera@gmail.com>
2018-10-09 10:32:46 +0200

    intent implemented!

--- app/src/main/java/com/utad/helloworld/FormActivity.java
+++ app/src/main/java/com/utad/helloworld/FormActivity.java
@@ -1,5 +1,8 @@
 package com.utad.helloworld;
 
+import android.arch.lifecycle.Observer;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,8 +14,10 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class FormActivity extends AppCompatActivity {
+import java.util.Date;
 
+public class FormActivity extends AppCompatActivity {
+    private DatePickerFragment datePickerFragment;
     @BindView(R.id.editText_name)  EditText m_et_name;
     @BindView(R.id.editText_firstSurname)  EditText m_et_firstUsername;
     @BindView(R.id.editText_SecondSurname)EditText m_et_secondUsername;
@@ -22,6 +27,14 @@ public class FormActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
         ButterKnife.bind(this);
+        Intent intent= getIntent();
+        if(intent != null){
+            intent.getStringExtra(HomeActivity.KEY_USER_NAME);
+            Toast toast = new Toast(this);
+            toast.setGravity(5,6,6);
+            toast.makeText(this, intent.getStringExtra(HomeActivity.KEY_USER_NAME),Toast.LENGTH_SHORT).show();
+        }
+
         Spinner spinner = (Spinner) findViewById(R.id.planets_spinner);
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -31,7 +44,17 @@ public class FormActivity extends AppCompatActivity {
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
-
+        datePickerFragment = new DatePickerFragment();
+        datePickerFragment.selectedDate.observe(this, new Observer<Date>() {
+            @Override
+            public void onChanged(@Nullable Date date) {
+                if(date != null){
+                     Toast toast = new Toast(FormActivity.this);
+                           toast.setGravity(5,6,6);
+                            toast.makeText(FormActivity.this,date.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
 
@@ -44,7 +67,7 @@ public class FormActivity extends AppCompatActivity {
     public void test(View view) {
         Toast toast = new Toast(this);
         toast.setGravity(5,6,6);
-        Toast.makeText(this,m_et_name.getText().toString(), Toast.LENGTH_SHORT).show();
+        toast.makeText(this,m_et_name.getText().toString(), Toast.LENGTH_SHORT).show();
 
     }
 }
--- app/src/main/java/com/utad/helloworld/HomeActivity.java
+++ app/src/main/java/com/utad/helloworld/HomeActivity.java
@@ -1,25 +1,35 @@
 package com.utad.helloworld;
 
+import android.arch.lifecycle.Observer;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.autofill.AutofillValue;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import java.util.Date;
+
+import static android.view.View.AUTOFILL_TYPE_DATE;
 
 public class HomeActivity extends AppCompatActivity {
-    private EditText m_et_email;
-    private EditText m_et_password;
+    public static  final String KEY_USER_NAME = "KEY_USER_NAME";
+    @BindView(R.id.editTextPass)EditText et_password;
+    @BindView(R.id.editTextEmail) EditText et_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        m_et_email = findViewById(R.id.editTextEmail);
-        m_et_password = findViewById(R.id.editTextPass);
-        String str_email = m_et_email.getText().toString();
-        String str_password = m_et_password.getText().toString();
+        ButterKnife.bind(this);
+
 
 
 
@@ -27,13 +37,19 @@ public class HomeActivity extends AppCompatActivity {
     public void onCheckboxClicked(View view) {
         Toast toast = new Toast(this);
         toast.setGravity(5,6,6);
-        Toast.makeText(this, "funciona", Toast.LENGTH_SHORT).show();
+        toast.makeText(this, "funciona", Toast.LENGTH_SHORT).show();
 
     }
     public void onLoginAction(View view) {
        Toast toast = new Toast(this);
         toast.setGravity(5,6,6);
-        Toast.makeText(this, "funciona", Toast.LENGTH_SHORT).show();
+
+
+            toast.makeText(this, "logged", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(HomeActivity.this, FormActivity.class);
+            intent.putExtra(KEY_USER_NAME, et_user.getText().toString());
+            startActivity(intent);
+
 
     }
 }
